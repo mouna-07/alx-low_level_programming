92912d6ff7df937b7c5471007ee322327b02d535
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-04-21 10:51:44 -0700

    Single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-	int n;
+int n;
 
-	for (n = 0; n <= 9; n++)
-	{
-	  putchar((n % 10) + '0');
-	  if (n != 9)
-	  {
-	     putchar(',');
-	     putchar(' ');
-	  }
-        }
-	putchar('\n');
+for (n = 0; n <= 9; n++)
+{
+putchar((n % 10) + '0');
+if (n != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
 
-	return (0);
+return (0);
 }
