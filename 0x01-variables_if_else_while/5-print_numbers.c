8ff715752a55483c459f59a9b83e8f19f190cfe0
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-04-21 08:49:39 -0700

    10 base numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints from 0 to 10.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
+
+	printf("\n");
+
+	return (0);
+}
