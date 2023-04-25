fb1e4655f5e4877de7683c90a6dd404d77385b50
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-04-25 13:21:41 -0700

    Function Sum()

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @m: int to be added to n
+ * @n: int to be added to m
+ * Return: sum of a and b
+ */
+int add(int m, int n)
+{
+	int s;
+
+	s = m + n;
+
+	return (s);
+}
