9861f8789b30843281fc296e1393cbfc38ddf434
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-04-25 13:03:13 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @m: the int to check
+ * Return: the absolute value of m
+ */
+int _abs(int m)
+{
+	if (m >= 0)
+	{
+		return (m);
+	}
+	return (-m);
+}
