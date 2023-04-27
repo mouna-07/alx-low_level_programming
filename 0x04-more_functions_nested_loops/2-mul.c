8b3defc0043ef098c5a4ae04e984bbf3a6ab43ce
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-04-27 13:45:59 -0700

    multiplication function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul -  checks for checks for a digit (0 through 9).
+ * @a: m -  Variable
+ * @b: n - variable
+ * Return: Always 0.
+ */
+int mul(int m, int n)
+{
+	int mul;
+
+	mul = m * n;
+
+	return (mul);
+}
