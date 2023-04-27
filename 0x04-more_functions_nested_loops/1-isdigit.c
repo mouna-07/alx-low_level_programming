8d681da7b68bba2ec88f098b3ca748384595240d
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-04-27 13:42:49 -0700

    Is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @m: int to be checked
+ * Return: 1 if m is a digit, 0 otherwise
+ */
+int _isdigit(int m)
+{
+	return (m >= '0' && m <= '9');
+}
