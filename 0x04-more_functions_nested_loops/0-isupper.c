663b996a6e30300300555a06762e8bd567f502bf
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-04-27 13:33:26 -0700

    C

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase character
+* @num: Variable text
+* Return: Always 0.
+*/
+int _isupper(int num)
+{
+
+
+	if (num >= 'A' && num <= 'Z')
+	{
+	return (1);
+	}
+	else
+{
+	return (0);
+}
+
+}
