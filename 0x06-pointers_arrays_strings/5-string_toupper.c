ba17ff3a5643d92fc2bb24774d997c6af8811dd0
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-05-04 04:10:45 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - switch from lowercase to uppercase
+ * @p: pointeur
+ *
+ * Return: o
+ */
+char *string_toupper(char *o)
+{
+	int i;
+
+	i = 0;
+	while (o[i] != '\0')
+	{
+		if (o[i] >= 'a' && o[i] <= 'z')
+			o[i] = o[i] - 32;
+		i++;
+	}
+	return (o);
+}
