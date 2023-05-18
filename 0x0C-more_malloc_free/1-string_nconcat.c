7a9cefe8fc822713821d4d32fef2ced4aee58848
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-05-18 11:51:49 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,24 +3,24 @@
 
 /**
  * *string_nconcat - concatene un nb de bytes de str a un str
- * @str1: str destination
- * @str2: st source
- * @nb: nombre de bytes concatenes
+ * @s1: str destination
+ * @s2: st source
+ * @n: nombre de bytes concatenes
  *
  * Return: pointer to the resulting string
  */
-char *string_nconcat(char *str1, char *str2, unsigned int nb)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
 	unsigned int a = 0, b = 0, l1 = 0, l2 = 0;
 
-	while (str1 && str1[l1])
+	while (s1 && s1[l1])
 		l1++;
-	while (str2 && str2[l2])
+	while (s2 && s2[l2])
 		l2++;
 
-	if (nb < l2)
-		str = malloc(sizeof(char) * (l1 + nb + 1));
+	if (n < l2)
+		str = malloc(sizeof(char) * (l1 + n + 1));
 	else
 		str = malloc(sizeof(char) * (l1 + l2 + 1));
 
@@ -29,15 +29,15 @@ char *string_nconcat(char *str1, char *str2, unsigned int nb)
 
 	while (a < l1)
 	{
-		str[a] = str1[a];
+		str[a] = s1[a];
 		a++;
 	}
 
-	while (nb < l2 && a < (l1 + nb))
-		str[a++] = str2[b++];
+	while (n < l2 && a < (l1 + n))
+		str[a++] = s2[b++];
 
-	while (nb >= l2 && a < (l1 + l2))
-		str[a++] = str2[b++];
+	while (n >= l2 && a < (l1 + l2))
+		str[a++] = s2[b++];
 
 	str[a] = '\0';
 
