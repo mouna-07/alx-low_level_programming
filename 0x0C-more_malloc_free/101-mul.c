f7633b17d237b216c8675a1480dee87e87639f30
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-05-18 13:46:03 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,102 +1,114 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-
-#define ERR_MSG "Error"
+#include <string.h>
 
 /**
- * is_digit - verifies if a string contains a non-digit char
- * @str: evaluated string
+ * _isdigit - checks if character is digit
+ * @c: the character to check
  *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * Return: 1 if digit, 0 otherwise
  */
-int is_digit(char *str)
+int _isdigit(int c)
 {
-	int j = 0;
-
-	while (str[j])
-	{
-		if (str[j] < '0' || str[j] > '9')
-			return (0);
-		j++;
-	}
-	return (1);
+	return (c >= '0' && c <= '9');
 }
 
 /**
- * _strlen - returns the length str
- * @str: evaluated string
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: the length
+ * Return: integer length of string
  */
-int _strlen(char *str)
+int _strlen(char *s)
 {
-	int j = 0;
+	int i = 0;
 
-	while (str[j] != '\0')
-	{
-		j++;
-	}
-	return (j);
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
- * errors - handles errors for main
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * main - multiplies positive number
- * @argc: num of args
- * @argv: tab of args
+ * big_multiply - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
  *
- * Return: always 0 (Success)
+ * Return: the product big number string
  */
-int main(int argc, char *argv[])
+char *big_multiply(char *s1, char *s2)
 {
-	char *str1, *str2;
-	int l1, l2, l, j, c, d1, d2, *res, A = 0;
+	char *r;
+	int l1, l2, a, b, c, x;
 
-	str1 = argv[1], str2 = argv[2];
-	if (argc != 3 || !is_digit(str1) || !is_digit(str2))
-		errors();
-	l1 = _strlen(str1);
-	l2 = _strlen(str2);
-	l = l1 + l2 + 1;
-	res = malloc(sizeof(int) * l);
-	if (!res)
-		return (1);
-	for (j = 0; j <= l1 + l2; j++)
-		res[j] = 0;
-	for (l1 = l1 - 1; l1 >= 0; l1--)
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	r = malloc(a = x = l1 + l2);
+	if (!r)
+		printf("Error\n"), exit(98);
+	while (a--)
+		r[a] = 0;
+
+	for (l1--; l1 >= 0; l1--)
 	{
-		d1 = str1[l1] - '0';
-		c = 0;
-		for (l2 = _strlen(str2) - 1; l2 >= 0; l--)
+		if (!_isdigit(s1[l1]))
 		{
-			d2 = str2[l2] - '0';
-			c += res[l1 + l2 + 1] + (d1 * d2);
-			res[l1 + l2 + 1] = c % 10;
+			free(r);
+			printf("Error\n"), exit(98);
+		}
+		a = s1[l1] - '0';
+		c = 0;
+
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			if (!_isdigit(s2[l2]))
+			{
+				free(r);
+				printf("Error\n"), exit(98);
+			}
+			b = s2[l2] - '0';
+
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
+
 			c /= 10;
 		}
-		if (c > 0)
-			res[l1 + l2 + 1] += c;
+		if (c)
+			r[l1 + l2 + 1] += c;
 	}
-	for (j = 0; j < l - 1; j++)
-	{
-		if (res[j])
-			A = 1;
-		if (A)
-			_putchar(res[j] + '0');
-	}
-	if (!A)
-		_putchar('0');
-	_putchar('\n');
-	free(res);
-	return (0);
+	return (r);
 }
 
+
+/**
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ *
+ * Return: Always 0 on success.
+ */
+int main(int argc, char **argv)
+{
+	char *r;
+	int a, c, x;
+
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	r = big_multiply(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+	while (c < x)
+	{
+		if (r[c])
+			a = 1;
+		if (a)
+			_putchar(r[c] + '0');
+		c++;
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(r);
+	return (0);
+}
