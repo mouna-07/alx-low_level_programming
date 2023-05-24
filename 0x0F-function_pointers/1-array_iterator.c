014ccec365e19fb0cca42a92f85395f21b639df6
Mouna SIF-EDDINE <mounasifeddine7@gmail.com>
2023-05-24 08:51:32 -0700

    1. If you spend too much time thinking about a thing, youll never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - affiche les elem dun tab 
+ * @tab: tableau
+ * @elem: elements a afficher
+ * @ap: pointer to print in regular or hex
+ * Return: void
+ */
+void array_iterator(int *tab, size_t elem, void (*ap)(int))
+{
+	unsigned int i;
+
+	if (tab == NULL || ap == NULL)
+		return;
+
+	for (i = 0; i < elem; i++)
+	{
+		ap(tab[i]);
+	}
+}
